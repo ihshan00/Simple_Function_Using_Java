ab1f73f5d0b041080faf9137f9673acd7b82048e
ihshan00 <it20078614@my.sliit.lk>
2022-03-17 01:28:59 +0530

    Function Addded

--- src/Program.java
+++ src/Program.java
@@ -3,7 +3,14 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Testing the function");
+		System.out.println(cal(5,6));
+		
 
+	}
+	public static int cal(int a,int b) {
+		return a+b;
 	}
 
 }
+
+
